4641960821452ab9f9c8a66325010baa319041ad
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-10-20 19:16:57 +0530

    image compression added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { compressAccurately, downloadFile } from "image-conversion";
 function App() {
+  const [current, setCurrent] = useState("");
+  const wait = (name) => {
+    return new Promise((resolve) => {
+      return setTimeout(() => resolve(name), 100);
+    });
+  };
+  const onFileChange = async () => {
+    let files = document.getElementById("file").files;
+    for (let i = 0; i < files.length; i++) {
+      let fileName = await wait(i);
+      console.log(fileName);
+      await compressAccurately(files[i], 300).then((res) => {
+        setCurrent(files[i].name);
+        downloadFile(res, files[i].name);
+      });
+    }
+    setCurrent("Completed");
+  };
   return (
-    <div>
-      <h1>Hello</h1>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <input type="file" id="file" multiple onChange={onFileChange} />
+      <p>{current}</p>
     </div>
   );
 }
